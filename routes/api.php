ea45ad85bb1c10b3a7fd133032b6804761d158b8
Khaled Hesham <khaledhesham22@gmail.com>
2020-06-27 13:22:12 +0200

    Added user API routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('users')->group(function () {
+    Route::post('login', 'UserController@login');
+    Route::post('register', 'UserController@register');
+    Route::post('refreshtoken', 'AuthController@refreshToken');
+
+    Route::group(['middleware' => ['auth:api']], function () {
+        Route::post('logout', 'AuthController@logout');
+        Route::post('details', 'UserController@details');
+    });
 });
