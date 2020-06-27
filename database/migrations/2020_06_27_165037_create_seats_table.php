b7f26f75e82582f4f13b0328447ca31bddfc108f
Khaled Hesham <khaledhesham22@gmail.com>
2020-06-27 19:25:46 +0200

    Create seats migration

---
+++ database/migrations/2020_06_27_165037_create_seats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seats', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('bus_id');
+            $table->integer('row');
+            $table->integer('column');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seats');
+    }
+}
